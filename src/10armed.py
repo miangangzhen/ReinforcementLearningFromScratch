d73a65fc7e28a8fb44f95c91b63fc90b596c8e53
miangangzhen <miangangzhen@sina.com>
2019-04-09 16:58:51 +0800

    Add: UCB method for k-armed testbed

--- src/10armed.py
+++ src/10armed.py
@@ -30,31 +30,39 @@ class Env(object):
         return np.random.normal(loc=q, scale=0.5)
 
 
-class Gready(object):
-    """e-greedy method"""
-    def __init__(self, k=10, init_value=0.0, epsilon=0.0):
+class Agent(object):
+    """methods: e-greedy method, ucb, gradient"""
+    def __init__(self, k=10, init_value=0.0, epsilon=0.0, method="e-greedy"):
         self.value_function = np.array([init_value] * k, dtype=np.float32)
         self.k = k
         self.step = 1
         self.epsilon = epsilon
         self.action_selected_count = {}
+        self.method = method
+        self.action_count = np.zeros_like(self.value_function, dtype=np.int32)
 
     def guess(self):
         # exploration or exploitation
         if np.random.random() < self.epsilon:
             return np.random.choice(self.k)
-        action = np.argmax(self.value_function)
-        check = np.where(self.value_function == action)[0]
+        action = None
+        if self.method == "e-greedy":
+            action = np.argmax(self.value_function)
+        elif self.method == "ucb":
+            action = np.argmax(self.value_function + 2 * np.sqrt(np.log(self.step) / (self.action_count + 1e-5)))
+        check = np.where(self.value_function == self.value_function[action])[0]
         if len(check) == 0:
             return action
         else:
             return np.random.choice(check)
 
-    def update(self, action, reward):
+    def update(self, action, reward, *args):
+        self.action_count[action] += 1
+
         self.action_selected_count[action] = self.action_selected_count.get(action, 0) + 1
         action_value_before = self.value_function[action]
         self.value_function[action] = self.value_function[action] + (1 / self.step) * (reward - self.value_function[action])
-        print("action {} changed from {} to {}".format(action, action_value_before, self.value_function[action]))
+        # print("action {} changed from {} to {}".format(action, action_value_before, self.value_function[action]))
         self.step += 1
 
 
@@ -68,26 +76,40 @@ def train(agent, env):
     return reward_receive
 
 
-def plot_mean_reward(reward_receive):
+def plot_mean_reward(reward_receive, title):
     mean_averaged_reward_receive = []
     for i in range(len(reward_receive)):
-        start = max(0, i - 20)
-        end = min(len(reward_receive) - 1, i + 20)
+        start = max(0, i - 50)
+        end = min(len(reward_receive) - 1, i + 50)
         mean_averaged_reward_receive.append(np.mean(reward_receive[start: end]))
     plt.plot(mean_averaged_reward_receive)
+    plt.title(title)
     plt.show()
 
 if __name__ == "__main__":
     k = 10
     env = Env(k=k)
-    gready = Gready(k=k, init_value=1.0, epsilon=0.1)
 
-    reward_receive = train(gready, env)
+    # e-greedy
+    agent = Agent(k=k, init_value=1.0, epsilon=0.1)
+    reward_receive = train(agent, env)
 
-    plot_mean_reward(reward_receive)
+    # plot_mean_reward(reward_receive, "e-greedy")
 
     print("q_a_real")
     print(env.q_a_real)
     print("value_function")
-    print(gready.value_function)
-    print(gready.action_selected_count)
+    print(agent.value_function)
+    print(agent.action_selected_count)
+
+    # ucb
+    agent = Agent(k=k, init_value=1.0, epsilon=0.0, method="ucb")
+    reward_receive = train(agent, env)
+
+    # plot_mean_reward(reward_receive, "ucb")
+
+    print("q_a_real")
+    print(env.q_a_real)
+    print("value_function")
+    print(agent.value_function)
+    print(agent.action_selected_count)
